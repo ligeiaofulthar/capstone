8f84c0f94489d6b37c2692b8c43d4446b30379c0
ligeiaofulthar <office@juliahalper.at>
2021-03-18 13:06:53 +0100

    feat: add eventListener for form

--- src/client/index.js
+++ src/client/index.js
@@ -1,14 +1,15 @@
-import { getData } from './js/app'
+import { getGeonames } from './js/app'
 // import { handleSubmit } from './js/formHandler'
-
 // import img from './img/yulia_logo.svg'
 
 import './../client/styles/appStyles.scss';
 
+document.getElementById("submit-form").addEventListener('click', getGeonames);
+
 console.log("CHANGE!!");
 
 export {
-    getData,
+    getGeonames,
     // handleSubmit,
     // img
 }
