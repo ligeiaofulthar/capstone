d02e4d054f5661c62556b72e4e070d07ac82cf06
ligeiaofulthar <office@juliahalper.at>
2021-03-25 23:50:18 +0100

    feat: updateUI and countdown

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -6,31 +6,108 @@ function getGeonames(event) {
     console.log(newCity);
     let newCountry = document.getElementById('country').value;
     console.log(newCountry);
-    let travelDate = document.getElementById("travel-date").value;
-    console.log(travelDate);
-
+    let travelDate = new Date(document.getElementById("travel-date").value);
+    console.log("travelDate", travelDate);
     console.log("::: Form Submitted :::")
-    fetchData(newCity, newCountry)
+    
+    const diff = calcTravelTime(travelDate);
+    console.log(diff);
+    fetchSingleTrip(newCity, newCountry, diff);    
+    // calcTravelTime(travelDate);
+    diff;
 };
 
-const fetchData = async(newCity = '', newCountry = '') => {
-    const result = await fetch('http://localhost:5000/geonames', {
+const fetchAllTrips = async(diff) => {
+    if (diff <= 7) {
+        try {
+            const result = await fetch('http://localhost:5000/trips')
+            const allResults = await result.json()
+            console.log('allresults', allResults)
+
+            allResults.forEach(trip => {
+            const {coordinates, weather, image} = trip
+console.log("info so new", coordinates, weather, image);
+            document.getElementById('weather-info').innerHTML = `The current temperature is ${weather.temp}°C and the felt temperature is ${weather.feels}°C, ${weather.description}`;
+            document.getElementById('weather-icon').innerHTML = `<img src="../src/client/media/icons/${weather.icon}.png">`;
+
+            document.getElementById('location-img').innerHTML = `<img src="${image.image_url}">`;
+            // document.getElementById('date-info').innerHTML = `Weather info: ${weather.min} ${weather.max} ${weather.description}`;
+        })
+    } catch(error) {
+        console.log('error from fetchAllTrips in app.js', error);
+    }
+    }
+
+    else if (diff > 7 && diff <= 16){
+
+        try {
+            const result = await fetch('http://localhost:5000/trips')
+            const allResults = await result.json()
+            console.log('allresults', allResults)
+
+            allResults.forEach(trip => {
+            const {coordinates, weather, image} = trip
+
+            document.getElementById('weather-info').innerHTML = `Minimum temperature is ${weather.min}°C and maximum temperature is ${weather.max}°C, ${weather.description}`;
+            document.getElementById('weather-icon').innerHTML = `<img src="../src/client/media/icons/${weather.icon}.png">`;
+
+            document.getElementById('location-img').innerHTML = `<img src="${image.image_url}">`;
+            // document.getElementById('date-info').innerHTML = `Weather info: ${weather.min} ${weather.max} ${weather.description}`;
+        })
+        } catch(error) {
+            console.log('error from fetchAllTrips in app.js', error);
+        }
+    }
+}
+
+const fetchSingleTrip = async(newCity = '', newCountry = '', diff = '') => {
+    const result = await fetch('http://localhost:5000/trip', {
         method: 'POST',
-        mode: 'cors',
-        credentials: 'same-origin',
+        // mode: 'cors',
+        // credentials: 'same-origin',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ newCity, newCountry }),
+        body: JSON.stringify({ newCity, newCountry, diff }),
         })
-    try {
-        const apiAnswer = await result.json()
-        console.log('JSON:', apiAnswer)
-        return apiAnswer;
+        console.log(result)
 
-    } catch(error) {
-        console.log('error from fetchData in app.js', error);
-    }
+        if (!result.ok) {
+
+        } else {
+            fetchAllTrips(diff)
+        }
+    // try {
+    //     const apiAnswer = await result.json()
+    //     console.log('JSON:', apiAnswer)
+    //     return apiAnswer;
+
+    // } catch(error) {
+    //     console.log('error from fetchSingleTrip in app.js', error);
+    // }
+}
+
+function calcTravelTime(travelDate) {
+    let today = new Date();
+
+    // let todayTimeZone = new Intl.DateTimeFormat().format(today);
+    // console.log("timezone", todayTimeZone);
+
+    // let travelTimeZone = new Intl.DateTimeFormat().format(travelDate);
+    // console.log("traveltimezone", travelTimeZone);
+    
+    const msPerDay = 24 * 60 * 60 * 1000; // Number of milliseconds per day
+
+    let daysAway = (travelDate.getTime() - today.getTime())/msPerDay;
+    daysAway = Math.floor(daysAway);
+    let dateInfo = document.getElementById('date-info');
+    dateInfo.innerHTML = "";
+
+    const p = document.createElement("P");
+    p.innerHTML = `Your trip is ${daysAway} days away`;
+    dateInfo.appendChild(p);
+
+    return daysAway;
 }
 
 export { getGeonames }
